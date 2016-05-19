ba79d5d20f1d9e1f0850dc9b03d1bb435ef2d240
liuxu <liuxu@liuxudeMacBook-Pro.local>
2016-05-19 14:03:59 +0800

    update

--- iledong/ledong/AllTeam/CreateTeamVController.m
+++ iledong/ledong/AllTeam/CreateTeamVController.m
@@ -11,7 +11,12 @@
 #define kOrighHeight 64
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
-@interface CreateTeamVController ()
+@interface CreateTeamVController ()<UIScrollViewDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate> {
+    @private
+    CGFloat _mainScrollViewContentHeight;
+    CGFloat _mainScrollViewContentOffsetY;
+    BOOL _keyboardShow;
+}
 
 @property (weak, nonatomic) IBOutlet UIScrollView *mainScrollView;
 @property (weak, nonatomic) IBOutlet UIView *headerView;
@@ -22,6 +27,12 @@
 @property (weak, nonatomic) IBOutlet UIView *teamOtherInfoView;
 @property (weak, nonatomic) IBOutlet UIView *teamAuditView;
 @property (weak, nonatomic) IBOutlet UIButton *StartBtn;
+@property (weak, nonatomic) IBOutlet UITextField *teamNameTextField;
+@property (weak, nonatomic) IBOutlet UITextView *teamIntrodectionTextView;
+@property (weak, nonatomic) IBOutlet UITextField *maxCountTextField;
+@property (strong, nonatomic) IBOutlet UITextField *addTagTextField;
+@property (weak, nonatomic) IBOutlet UISwitch *switchCtl;
+
 
 @end
 
@@ -29,50 +40,179 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.navigationItem.title = @"创建团队";
-    self.navigationController.navigationBarHidden = NO;
-    self.tabBarController.tabBar.hidden = YES;
-    self.automaticallyAdjustsScrollViewInsets = NO;
+
+    [self setupNavigationBar];
     [self layoutSubView];
+    [self addKeyBoardNotification];
+    [self setupTapGestureRecognizer];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
-#pragma mark - draw UI
-- (void)layoutSubView {
-    CGFloat originY = 10;
-    self.mainScrollView.backgroundColor = UIColorFromRGB(0xF2F3F4);
-    self.mainScrollView.frame = CGRectMake(0, 0, APP_WIDTH, APP_HEIGHT);
-    
-    NSString* path = [[NSBundle mainBundle]pathForResource:@"img_teamavatar_120@2x" ofType:@"png"];
-    self.headerImageView.image = [UIImage imageWithContentsOfFile:path];
-    [self.headerImageView sizeToFit];
-
-    self.headerImageView.frame = CGRectMake(APP_WIDTH/2-self.headerImageView.frame.size.width/2, originY, self.headerImageView.frame.size.width, self.headerImageView.frame.size.height);
-    originY = self.headerImageView.frame.origin.y + self.headerImageView.frame.size.height + 15;
-    self.uploadHeaderImgBtn.frame = CGRectMake(APP_WIDTH/2-self.uploadHeaderImgBtn.frame.size.width/2, originY, self.uploadHeaderImgBtn.frame.size.width, self.uploadHeaderImgBtn.frame.size.height);
-    self.uploadHeaderImgBtn.layer.borderWidth = 1.0;
-    self.uploadHeaderImgBtn.layer.borderColor = UIColorFromRGB(0xDEDEDE).CGColor;
-    
-    self.headerView.frame = CGRectMake(0, kOrighHeight, APP_WIDTH, self.uploadHeaderImgBtn.frame.origin.y+self.uploadHeaderImgBtn.frame.size.height+10);
-    originY = self.headerView.frame.origin.y + self.headerView.frame.size.height + 10;
-    self.teamNameView.frame = CGRectMake(0, originY, APP_WIDTH, 55);
-    originY = self.teamNameView.frame.origin.y + self.teamNameView.frame.size.height + 10;
-    self.teamIntroductionView.frame = CGRectMake(0, originY, APP_WIDTH, 190);
-    originY = self.teamIntroductionView.frame.origin.y + self.teamIntroductionView.frame.size.height + 10;
-    self.teamOtherInfoView.frame = CGRectMake(0, originY, APP_WIDTH, 130);
-    originY = self.teamOtherInfoView.frame.origin.y + self.teamOtherInfoView.frame.size.height + 10;
-    self.teamAuditView.frame = CGRectMake(0, originY, APP_WIDTH, 60);
-    originY = self.teamAuditView.frame.origin.y + self.teamAuditView.frame.size.height + 10;
-    self.StartBtn.frame = CGRectMake(0, originY, APP_WIDTH, 40);
-    originY = self.StartBtn.frame.origin.y + self.StartBtn.frame.size.height;
-    
-    self.mainScrollView.contentSize = CGSizeMake(APP_WIDTH, originY);
+- (void)dealloc {
+    [self removeKeyBoardNotification];
 }
 
+#pragma mark - draw UI
+- (void)setupNavigationBar {
+    self.navigationController.navigationBarHidden = NO;
+    self.tabBarController.tabBar.hidden = YES;
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    UIBarButtonItem *backItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"top_back"] style:UIBarButtonItemStylePlain target:self action:@selector(backBtnClicked)];
+    backItem.tintColor = [UIColor redColor];
+    self.navigationItem.leftBarButtonItem = backItem;
+    UILabel *customLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 30)];
+    [customLab setTextColor:[UIColor redColor]];
+    [customLab setText:@"创建团队"];
+    customLab.font = [UIFont systemFontOfSize:16];
+    self.navigationItem.titleView = customLab;
+}
 
+- (void)layoutSubView {
+    self.uploadHeaderImgBtn.layer.borderWidth = 1.0;
+    self.uploadHeaderImgBtn.layer.borderColor = UIColorFromRGB(0xDEDEDE).CGColor;
+    self.addTagTextField = [[UITextField alloc]initWithFrame:CGRectMake(15, self.teamOtherInfoView.bounds.size.height - 30, 0, 0)];
+    self.addTagTextField.placeholder = @"添加标签";
+    self.addTagTextField.font = [UIFont systemFontOfSize:14.0];
+    self.addTagTextField.textColor = [UIColor redColor];
+    [self.addTagTextField sizeToFit];
+    [self.teamOtherInfoView addSubview:self.addTagTextField];
+    
+    self.switchCtl.onTintColor = [UIColor redColor];
+}
 
+- (void)addKeyBoardNotification {
+    _keyboardShow = NO;
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardDidHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+}
+
+- (void)removeKeyBoardNotification {
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillShowNotification
+                                                  object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+}
+
+- (void)setupTapGestureRecognizer {
+    UITapGestureRecognizer *tapGes = [[UITapGestureRecognizer alloc] init];
+    [tapGes addTarget:self action:@selector(endEditing)];
+    [self.mainScrollView addGestureRecognizer:tapGes];
+}
+
+#pragma mark - btn Clicked
+- (void)backBtnClicked {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)endEditing {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)uploadImageBtnClicked:(id)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]
+        || [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum]) {
+        //支持图库、相片库
+        UIImagePickerController* picker = [[UIImagePickerController alloc]init];
+        picker.view.backgroundColor = [UIColor whiteColor];
+        UIImagePickerControllerSourceType sourcheType = UIImagePickerControllerSourceTypePhotoLibrary|UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+        picker.sourceType = sourcheType;
+        picker.delegate = self;
+        picker.allowsEditing = YES;
+        [self presentViewController:picker animated:YES completion:nil];
+    }
+    else {
+        [SVProgressHUD showErrorWithStatus:@"不支持获取图库及相片库，请设置权限"];
+    }
+}
+
+- (void)keyboardWillShow:(NSNotification *) notif {
+    if (_keyboardShow)
+        return;
+
+    NSDictionary *info = [notif userInfo];
+    NSValue *value = [info objectForKey:UIKeyboardFrameEndUserInfoKey];
+    CGSize keyboardSize = [value CGRectValue].size;
+    
+    _mainScrollViewContentHeight = self.mainScrollView.contentSize.height;
+    _mainScrollViewContentOffsetY = self.mainScrollView.contentOffset.y;
+    
+    CGFloat y = 0;
+    if ([self.teamNameTextField isFirstResponder])
+        y = self.teamNameView.frame.origin.y - kOrighHeight;
+    else if ([self.teamIntrodectionTextView isFirstResponder])
+        y = self.teamIntroductionView.frame.origin.y - kOrighHeight;
+    else if ([self.maxCountTextField isFirstResponder])
+        y = self.teamOtherInfoView.frame.origin.y - kOrighHeight;
+    else if ([self.addTagTextField isFirstResponder])
+        y = self.teamAuditView.frame.origin.y - kOrighHeight - 80;
+    CGFloat sizeHeigth = _mainScrollViewContentHeight + keyboardSize.height;
+    [UIView animateWithDuration:0.5 animations:^{
+        [self.mainScrollView setContentSize:CGSizeMake(APP_WIDTH, sizeHeigth)];
+        [self.mainScrollView setContentOffset:CGPointMake(0, y) animated:YES];
+    } completion:nil];
+}
+
+- (void)keyboardDidHide:(NSNotification *) notif {
+    _keyboardShow = NO;
+    [UIView animateWithDuration:0.5 animations:^{
+        [_mainScrollView setContentSize:CGSizeMake(APP_WIDTH, _mainScrollViewContentHeight)];
+        [_mainScrollView setContentOffset:CGPointMake(0, _mainScrollViewContentOffsetY) animated:YES];
+    } completion:nil];
+}
+
+- (void)uploadHeaderImage {
+    if ([HttpClient isLogin]) {
+        NSString *urlStr = [API_BASE_URL stringByAppendingString:API_UPLOAD_HEADERIMAGE_URL];
+        NSDictionary *dic = @{@"token":[HttpClient getTokenStr],};
+                              //@"file":};
+        [HttpClient postJSONWithUrl:urlStr parameters:dic success:^(id responseObject) {
+            NSDictionary* dict = (NSDictionary*)responseObject;
+            NSNumber* codeNum = [dict objectForKey:@"code"];
+            if (codeNum.intValue == 0) {
+                [Dialog alert:@"上传团队头像成功！"];
+            }
+            else {
+                [Dialog alert:@"上传团队头像失败！"];
+            }
+        } fail:^{
+            [Dialog alert:@"上传团队头像失败！"];
+        }];
+    }
+}
+
+#pragma mark - delegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    __weak typeof(self) weakSelf = self;
+    [picker dismissViewControllerAnimated:YES completion:^{
+        UIImage* image = [info objectForKey:UIImagePickerControllerOriginalImage];
+        CGRect rect = weakSelf.headerImageView.frame;
+        UIGraphicsBeginImageContext(CGSizeMake(rect.size.width, rect.size.height));
+        [image drawInRect:CGRectMake(0, 0, rect.size.width, rect.size.height)];
+        UIImage *smallImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        image = smallImage;
+        weakSelf.headerImageView.image = image;
+        
+        //API_UPLOAD_HEADERIMAGE_URL
+        
+        
+    }];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
