1ff99b230036cd96b61fe896c44e2ada0c6d5ad2
djcxym <djcxym@126.com>
2016-05-20 17:30:20 +0800

    update

---
+++ iledong/ledong/PersonalCenter/TradeDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TradeDetailViewController.h
+//  ledong
+//
+//  Created by dengjc on 16/5/20.
+//  Copyright © 2016年 LeDong. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface TradeDetailViewController : BaseViewController<UITableViewDelegate,UITableViewDataSource>
+
+@end
