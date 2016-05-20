4c888662a8f22f78b94e5441dd2749a3a2061b6a
liuxu <liuxu@liuxudeMacBook-Pro.local>
2016-05-20 16:03:36 +0800

    update

--- iledong/ledong/AllTeam/活动管理/ActiveTableViewCell.h
+++ iledong/ledong/AllTeam/活动管理/ActiveTableViewCell.h
@@ -8,6 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum activityState {
+    activityStateOnGoing = 0,
+    activityStateEnd
+}activityState;
+
+typedef void (^managerBtnClickedBlock)();
+
 @interface ActiveTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *activityImageView;
+@property (weak, nonatomic) IBOutlet UILabel *activityName;
+@property (weak, nonatomic) IBOutlet UILabel *activityDesc;
+@property (weak, nonatomic) IBOutlet UILabel *activityState;
+
+@property (assign, nonatomic)activityState state;
+- (void)setSelectManagerBtnClicked:(managerBtnClickedBlock)block;
 
 @end
