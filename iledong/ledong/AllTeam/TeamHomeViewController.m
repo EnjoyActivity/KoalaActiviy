1db4f031a1759071d7f9c118ac99dc335d2ef1cd
djcxym <djcxym@126.com>
2016-05-27 17:49:14 +0800

    团队主页

--- iledong/ledong/AllTeam/TeamHomeViewController.m
+++ iledong/ledong/AllTeam/TeamHomeViewController.m
@@ -181,7 +181,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return teamActivity.count;
+    return MAX(2, teamActivity.count);
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
